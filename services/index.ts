6421c3e852fbf6ba23efe90194eebafc61e73847
godmoonsnake <shashidharnag541@gmail.com>
2026-04-18 14:06:56 +0530

    Delete services directory

--- services/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ES Module wrapper for the foodInspection service.
- * This allows the CommonJS service to be imported by Next.js API routes.
- */
-
-// Re-export functions from the CommonJS module
-const { inspectFood, analyzeImageWithGemini, adjustForTemperature, calculateRiskScore } = require('./foodInspection.js');
-
-export { inspectFood, analyzeImageWithGemini, adjustForTemperature, calculateRiskScore };
